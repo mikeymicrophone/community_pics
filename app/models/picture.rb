b0ee7891639d2e5a4f40acb66d15f5383c2b4306
Mike Schwab <mike.schwab@gmail.com>
2012-01-18 10:11:14 -0500

    tagged pictures shown on tag show page; tags for picture shown on picture show page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,6 @@
 class Picture < ActiveRecord::Base
+  has_many :taggings
+  has_many :tags, :through => :taggings
+  
   mount_uploader :photo_file, PhotoUploader
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+  has_many :taggings
+  has_many :pictures, :through => :taggings
 end
