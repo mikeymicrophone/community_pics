73342241b279a146038efbc6c6ad62056bfa6c51
Mike Schwab <mike.schwab@gmail.com>
2012-04-10 18:34:34 -0400

    use google to store images

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider                         => 'Google',
+    :google_storage_access_key_id     => ENV['GOOGLE_KEY_ID'],
+    :google_storage_secret_access_key => ENV['GOOGLE_KEY']
+  }
+  config.fog_directory = 'ruby_class_photos'
+end
